7e68a613159200712e285a356769bfe964d28351
Nikhil <nikhil.ramamoorthy@securin.io>
2023-05-28 12:25:13 +0530

    Added maven project

---
+++ src/main/java/com/wipro/course/creation/CourseCreationApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.course.creation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseCreationApplication.class, args);
+	}
+
+}
